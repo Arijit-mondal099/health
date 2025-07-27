9806f5bcb001c205be613581cfd341bba3f8a16c
Arijit Mondal <arijitm717@gmail.com>
2025-07-27 10:40:13 +0530

    update corse setting

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,16 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "https://health-frontend-c4x4.onrender.com",
+      "https://health-admin-o5vg.onrender.com"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
