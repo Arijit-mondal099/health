5d233b6945e4d272b4d99be20bd390d5bc5b5362
Arijit Mondal <arijitm717@gmail.com>
2025-07-27 10:25:45 +0530

    update cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
