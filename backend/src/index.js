12dac3b86a910285bb1e1bed658887f8739e60ec
Arijit Mondal <arijitm717@gmail.com>
2025-07-27 12:25:11 +0530

    update corse setting

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,9 @@ app.use(
   cors({
     origin: [
       "https://health-frontend-c4x4.onrender.com",
-      "https://health-admin-o5vg.onrender.com"
+      "https://health-admin-o5vg.onrender.com",
+      "http://localhost:5173",
+      "http://localhost:5174",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
