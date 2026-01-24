859f3f7139ec6f456264d0584257e8a2a8ad4bd8
Arijit Mondal <arijitm717@gmail.com>
2026-01-24 18:43:12 +0530

    fixed database bug

--- backend/src/db/dbConnection.js
+++ backend/src/db/dbConnection.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
 export const dbConnection = async () => {
   try {
-    const res = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log("DB :: Data base connected successfully ->", res.connection.host);
+    const res = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    console.log("DB :: Database connected successfully ->", res.connection.host);
   } catch (err) {
-    console.log("DB :: Data base connection error ->", err.message);
+    console.log("DB :: Database connection error ->", err.message);
     process.exit(1);
   }
 };
